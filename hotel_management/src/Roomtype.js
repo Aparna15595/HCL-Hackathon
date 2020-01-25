837c262adcf932f3f0fba13f2186b8a800a25643
shivanishah <shah.shivani1993@gmail.com>
2020-01-25 11:38:22 +0000

    add changes in component

---
+++ hotel_management/src/Roomtype.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Checkbox from './Checkbox';
+
+class Roomtype extends React.Component{
+state = { checked: false }
+handleCheckboxChange = event =>
+    this.setState({ checked: event.target.checked })
+render() {
+    return (
+    <ul>
+        <li>
+        <label>
+        <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+        />
+        <span>AC</span>
+        </label>
+        </li>
+        <li>
+    <label>
+        <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+        />
+        <span>Non AC</span>
+        </label></li>
+    </ul>    
+    ) 
+}   
+}
+
+export default Roomtype;
