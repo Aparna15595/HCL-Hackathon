752fbaeb177088f5e1d3589d5a9c38bfd962c0f4
unknown <madireddyvishnupriya@gmail.com>
2020-01-25 12:10:43 +0000

    add search display component

--- hotel_management/src/Searchdisplay.js
+++ hotel_management/src/Searchdisplay.js
@@ -3,27 +3,46 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Filter from './components/filter';
 import './index.css';
+import displayData from './Mock_data/Data.json';
+import DataToDisplay from './DataToDisplay'
 
 class Searchdisplay extends React.Component{
-    render(){   
-        return (
-            <div id="search_display">
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-sm-3">
-                        <Filter/>
-                        <div className="text-center">
-                            
-                        </div>
-                        </div>
-                        <div className="col-sm-9">
-
-                        </div>
+constructor(props){
+    super(props);
+    this.state = {
+        value : {}
+    };
+}
+filteredData(){
+    let filteredData= displayData.Hotel_Details.map(data => {
+        console.log(data);
+    if(data.state == "Ongole"){
+        console.log(data);
+    } 
+})    
+}
+render(){   
+    return (
+        <div id="search_display">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-3">
+                    <Filter/>
+                    <div className="text-center">
+                        
+                    </div>
+                    </div>
+                    <div className="col-sm-9">
+                    <div> 
+            {this.filteredData}
+        <DataToDisplay filteredData = {this.state.value}/>
+        </div>
                     </div>
                 </div>
             </div>
-        );      
-    }
+        </div>
+    );      
+}
 }
 
 export default Searchdisplay;
